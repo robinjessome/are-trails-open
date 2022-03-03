66494cad00268af06494ed3eb630eb662032fd61
robin <rjessome@miovision.com>
2022-03-03 15:29:35 -0500

    add tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div className="bg-gray-300 p-2 rounded w-[500px] my-4 mx-auto">Welcome to Next.js!</div>
   }
   
   export default HomePage
