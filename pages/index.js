f2442b4695716c4d32cbe0e33a44b9d3063ff9f2
robin <rjessome@miovision.com>
2022-03-04 11:51:11 -0500

    readme & style tweaks

--- pages/index.js
+++ pages/index.js
@@ -14,22 +14,62 @@ function HomePage({trailStatusAPIId, trailName}) {
         setData(data)
         setLoading(false)
       })
-  }, [])
+  }, [trailStatusAPIId])
+
+  // const status = data?.status;
+  const message = data?.message
+  const updatedAt = data?.updatedAt;
+  const updatedDate = new Date(updatedAt);
+  const postLink = data?.instagramPermalink;
+  const username = data?.user.username;
+
+  let statusMessage, border;
+  if(data?.status === 'open') {
+    border = 'border-color-green-500';
+    statusMessage = 'Yes, '+trailName+' is <strong>Open</strong>!';
+  } else {
+    border = 'border-color-red-500';
+    statusMessage = 'No, '+trailName+' is closed...';
+  }
+
+  // if (isLoading) return <p>Loading...</p>
+  // if (!data) return <p>No profile data</p>
 
     return (
       <>
         <Head>
           <title>Is {trailName} open?</title>
         </Head>
-        <div>
-          <div className="text-center mb-4">
+        <div className="mt-auto">
+          <div className="text-center">
             <Image src="/img/hydrocut-logo_sm.png" width="200" height="140" alt="" />
           </div>
-          <h1 className="text-center text-5xl mb-12">{trailName} is <span className="font-bold">{data?.status}</span></h1>
-          <div className="text-center">
-            <p className="text-lg mb-4">{data?.message}</p>
-            <p className="text-sm">{data?.updatedAt}</p>
-          </div>
+          {/* {isLoading && <div className="mt-8 text-center">loading...</div>} */}
+          {!isLoading && (
+            <>
+              {statusMessage &&
+                <h1 className="text-center text-5xl mt-12 mb-8" dangerouslySetInnerHTML={{  __html: statusMessage }}></h1>
+              }
+              <div className="text-center">
+                {message &&
+                  <p className={`border text-lg mb-2 bg-white py-2 px-4 rounded`}>{message}</p>
+                }
+                <div className="flex justify-between items-center mt-2">
+                  {updatedAt && 
+                    <p className="text-xs text-slate-500">Last update: <strong>{updatedDate.toDateString()}</strong></p>
+                  }
+                  {postLink &&
+                    <a href={postLink} className="text-slate-500 hover:text-sky-600" target="_blank" rel="noreferrer">
+                      <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4 inline-block mr-1 relative -top-[1px]"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
+                        {username}
+                      </span>
+                    </a>
+                  }
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </>
     )
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Trails...are they open!?
+
+Just a little Next.js practice, leveraging an existing API endpoint from [trailstatusapp.com](https://trailstatusapp.com/) // [jschr/trail-status-app](https://github.com/jschr/trail-status-app) for [The Hydrocut](https://www.thehydrocut.ca/) mountain bikiing trails, in Kitchener, Ontario.
+
+##.env varaibles
+- `TRAIL_NAME` - for the trail name, obviously.
+- `TRAILSTATUS_API_ID` - trail id for trailstatusapp endpoint
