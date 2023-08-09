d734e9b6426c41e9f32223c1a0cde7b01a3706a3
AB <bourdeaux.adrien@gmail.com>
2023-08-08 20:39:00 -0400

    added assets + pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+});
+
+module.exports = withPWA({
     env: {
       trailName: 'the Hydrocut',
-    },
-  }
+    }
+  })
