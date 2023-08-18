97bc820212625c53f3c10f7b5e9fde8182e6f333
AB <bourdeaux.adrien@gmail.com>
2023-08-17 20:20:35 -0400

    started vercel kv support

--- pages/api/notification.js
+++ pages/api/notification.js
@@ -1,5 +1,24 @@
 const webPush = require('web-push')
 
+import { kv } from '@vercel/kv';
+ 
+async function exampleCommands() {
+  try {
+    await kv.set('setExample', 'abc123', { ex: 100, nx: true });
+  } catch (error) {
+    // Handle errors
+  }
+}
+ 
+async function exampleCommands2() {
+  try {
+    const getExample = await kv.get('setExample');
+    console.log(getExample);
+  } catch (error) {
+    // Handle errors
+  }
+}
+
 webPush.setVapidDetails(
   `mailto:${process.env.WEB_PUSH_EMAIL}`,
   process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY,
@@ -31,6 +50,8 @@ const Notification = (req, res) => {
     res.statusCode = 405
     res.end()
   }
+  exampleCommands();
+  exampleCommands2();
 }
 
 export default Notification
