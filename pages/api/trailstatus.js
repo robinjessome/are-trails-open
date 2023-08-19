29d8d40aa62057380c7b317616c01cc56145263f
AB <bourdeaux.adrien@gmail.com>
2023-08-19 14:43:43 -0400

    trailstatus api receives trail status, if it is newer than previous status, sends a push notification to users

---
+++ pages/api/trailstatus.js
@@ -0,0 +1,69 @@
+import { kv } from '@vercel/kv';
+const webPush = require('web-push')
+webPush.setVapidDetails(
+    `mailto:${process.env.WEB_PUSH_EMAIL}`,
+    process.env.NEXT_PUBLIC_WEB_PUSH_PUBLIC_KEY,
+    process.env.WEB_PUSH_PRIVATE_KEY
+)
+
+
+async function setKVvar(keyname, value) {
+    await kv.set(keyname, value)
+        .catch(error => { console.log("error in setting kv value: ", error); });
+    console.log("set ", keyname, " to ", value);
+}
+async function readKVvar(keyname) {
+    let kvvalue = await kv.get(keyname)
+        .catch(error => { console.log("error in getting kv value: ", error); });
+    console.log("found ", keyname, "'s value to be ", kvvalue);
+    return kvvalue;
+}
+
+async function sendNotifications(trailStatus) {
+    console.log("sending notifications");
+    const list = await kv.lrange('subs', 0, -1);
+    for (let i = 0; i < list.length; i++) {
+        var sub = list[i];
+        webPush
+            .sendNotification(
+                sub,
+                JSON.stringify({ title: 'Hello Web Push', message: 'Your web push notification is here!' })
+            )
+            .catch(err => {
+                console.error(err)
+            })
+    }
+}
+async function logic(req){
+    let lastUpdated = req.body.updatedAt;
+        let lastUpdatedKV = await readKVvar('lastUpdated')
+        console.log(lastUpdatedKV);
+        if (lastUpdatedKV != lastUpdated) {
+            await setKVvar('lastUpdated', lastUpdated);
+            await setKVvar('lastStatus', req.body);
+            await sendNotifications(req.body)
+}
+}
+const TrailStatus = (req, res) => {
+    if (req.method == 'POST') {
+        // TODO: take input from JSON request(trailstatus api fetch) and store in in a kv value
+        logic(req).
+                catch(err => {
+                    if ('statusCode' in err) {
+                        res.writeHead(err.statusCode, err.headers).end(err.body)
+                    } else {
+                        console.error(err)
+                        res.statusCode = 500
+                        res.end()
+                    }
+                })
+        res.statusCode = 200
+        res.end()
+    } else {
+        res.statusCode = 405
+        res.end()
+    }
+
+}
+
+export default TrailStatus
