43c423d7c7b60abbfb6059048630dd222d3ce937
AB <bourdeaux.adrien@gmail.com>
2023-09-02 17:51:50 -0400

    added icon button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head'
 import Image from 'next/image'
-import { IconCheck, IconX, IconBrandInstagram } from '@tabler/icons';
+import { IconCheck, IconX, IconBrandInstagram, IconBellOff, IconBellRingingFilled } from '@tabler/icons-react';
 
 const base64ToUint8Array = base64 => {
   const padding = '='.repeat((4 - (base64.length % 4)) % 4)
@@ -131,6 +131,11 @@ function HomePage({ trailStatusAPIId, trailName }) {
         <title>Is {trailName} open?</title>
       </Head>
       <div className="mt-auto text-slate-900 dark:text-slate-300 p-4 max-w-2xl">
+      <div className="text-right">
+       {isSubscribed 
+       ? <IconBellRingingFilled className="w-6 mr-1 inline-block relative -top-[1px]" onClick={unsubscribeButtonOnClick}/>
+       : <IconBellOff className="w-6 mr-1 inline-block relative -top-[1px]" onClick={subscribeButtonOnClick}/>}
+        </div>
         <div className="text-center">
           <Image src="/img/icons/logo.png" width="200" height="140" alt="" />
         </div>
@@ -139,6 +144,7 @@ function HomePage({ trailStatusAPIId, trailName }) {
             {statusMessage &&
               <h1 className="text-center text-5xl mt-12" dangerouslySetInnerHTML={{ __html: statusMessage }}></h1>
             }
+
             {data?.trails && (
               <div className="mt-6">
                 <p className="text-center">
@@ -188,32 +194,9 @@ function HomePage({ trailStatusAPIId, trailName }) {
           </>
         )}
       </div>
-      
-      <svg id = "filledBellIcon" onClick={unsubscribeButtonOnClick} xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bell-ringing-filled" width="60" height="60" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M17.451 2.344a1 1 0 0 1 1.41 -.099a12.05 12.05 0 0 1 3.048 4.064a1 1 0 1 1 -1.818 .836a10.05 10.05 0 0 0 -2.54 -3.39a1 1 0 0 1 -.1 -1.41z" strokeWidth="0" fill="currentColor"></path>
-   <path d="M5.136 2.245a1 1 0 0 1 1.312 1.51a10.05 10.05 0 0 0 -2.54 3.39a1 1 0 1 1 -1.817 -.835a12.05 12.05 0 0 1 3.045 -4.065z" strokeWidth="0" fill="currentColor"></path>
-   <path d="M14.235 19c.865 0 1.322 1.024 .745 1.668a3.992 3.992 0 0 1 -2.98 1.332a3.992 3.992 0 0 1 -2.98 -1.332c-.552 -.616 -.158 -1.579 .634 -1.661l.11 -.006h4.471z" strokeWidth="0" fill="currentColor"></path>
-   <path d="M12 2c1.358 0 2.506 .903 2.875 2.141l.046 .171l.008 .043a8.013 8.013 0 0 1 4.024 6.069l.028 .287l.019 .289v2.931l.021 .136a3 3 0 0 0 1.143 1.847l.167 .117l.162 .099c.86 .487 .56 1.766 -.377 1.864l-.116 .006h-16c-1.028 0 -1.387 -1.364 -.493 -1.87a3 3 0 0 0 1.472 -2.063l.021 -.143l.001 -2.97a8 8 0 0 1 3.821 -6.454l.248 -.146l.01 -.043a3.003 3.003 0 0 1 2.562 -2.29l.182 -.017l.176 -.004z" strokeWidth="0" fill="currentColor"></path>
-</svg>
-<svg id = "unfilledBellIcon" onClick={subscribeButtonOnClick} xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-bell" width="60" height="60" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-   <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6"></path>
-   <path d="M9 17v1a3 3 0 0 0 6 0v-1"></path>
-</svg>
-
     </>
   )
 }
-{/* <button onClick={subscribeButtonOnClick} disabled={isSubscribed}>
-Subscribe
-</button>
-<button onClick={unsubscribeButtonOnClick} disabled={!isSubscribed}>
-Unsubscribe
-</button>
-<button onClick={sendNotificationButtonOnClick} disabled={!isSubscribed}>
-Send Notification
-</button> */}
 
 export async function getStaticProps(context) {
   return {
